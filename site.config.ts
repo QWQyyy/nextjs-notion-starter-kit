ec6ea5ee76196d4b7bfa59ee8587cb155a7e1cbd
Xin Wen <54056797+QWQyyy@users.noreply.github.com>
2022-05-13 12:51:57 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'test-36dd718d7d9245ca8c06e19825d7ca6f',
+  rootNotionPageId: 'f4c8ff260e384439a384e750c3dfc574',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'test',
-  domain: 'https://www.notion.so/test-36dd718d7d9245ca8c06e19825d7ca6f',
+  domain: 'https://www.notion.so/f4c8ff260e384439a384e750c3dfc574',
   author: 'GOOD Lab',
 
   // open graph metadata (optional)
